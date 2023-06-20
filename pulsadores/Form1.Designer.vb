5c890cdfe7293f791d653cc5a37828aab2334935
David Owtschinnikow <dowtschinnikow@elt.es>
2023-06-20 21:49:16 +0200

    Inicio

---
+++ pulsadores/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.pic1 = New System.Windows.Forms.PictureBox()
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM5"
+        Me.SerialPort1.ReadTimeout = 1000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(37, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'pic1
+        '
+        Me.pic1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pic1.Image = CType(resources.GetObject("pic1.Image"), System.Drawing.Image)
+        Me.pic1.Location = New System.Drawing.Point(0, 0)
+        Me.pic1.Name = "pic1"
+        Me.pic1.Size = New System.Drawing.Size(1068, 610)
+        Me.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.pic1.TabIndex = 1
+        Me.pic1.TabStop = False
+        Me.pic1.Visible = False
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(783, 417)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(126, 66)
+        Me.AxWindowsMediaPlayer1.TabIndex = 2
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1068, 610)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Controls.Add(Me.pic1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Button1 As Button
+    Friend WithEvents pic1 As PictureBox
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ pulsadores/Form1.vb
@@ -0,0 +1,62 @@
+﻿Imports System.IO.Ports
+
+Public Class Form1
+    Sub PlayBackgroundSoundFile()
+        My.Computer.Audio.Play("C:\wtrfall.wav",
+        AudioPlayMode.WaitToComplete)
+    End Sub
+    Sub lionPlay()
+        My.Computer.Audio.Play("C:\lion2.wav",
+        AudioPlayMode.WaitToComplete)
+    End Sub
+    Dim lion As Byte = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            SerialPort1.PortName = "COM5"
+            SerialPort1.Open()
+            Timer1.Enabled = True
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Try
+            Dim Incoming As Byte
+            Incoming = SerialPort1.ReadByte()
+            'MessageBox.Show(Incoming.ToString)
+            If (Incoming = 162) Then
+                lion = 1
+
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If (lion = 1) Then
+                'pic1.Visible = True
+                'Application.DoEvents()
+                'lionPlay()
+                AxWindowsMediaPlayer1.URL = "C:\lion.wmv"
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If (lion = 1) Then
+            lion = 0
+
+            pic1.Visible = True
+            pic1.Enabled = False
+            Application.DoEvents()
+            lionPlay()
+            'Application.DoEvents()
+            'AxWindowsMediaPlayer1.URL = "C:\lion.gif"
+        End If
+    End Sub
+End Class
